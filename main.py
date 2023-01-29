7e64706d50ce63359e7cb92c830652920e9678fc
Junseok-Hur <j1hur@student.bridgew.edu>
2023-01-29 01:17:22 -0500

    save data into text file

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import json
 import base64
 
 
+def save_data(data, filename='wufoo.text'):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
+        file.close()
+
+
 def get_data():
     subdomain = 'veilside'
     key = secrets.key
@@ -17,9 +23,11 @@ def get_data():
         headers={'Authorization': 'Basic ' + base64.b64encode(f'{key}:{password}'.encode()).decode()}
     )
     data = json.loads(r.data.decode('utf-8'))
+    # print(json.dumps(data, indent=4, sort_keys=True))
 
-    print(json.dumps(data, indent=4, sort_keys=True))
+    return data
 
 
 if __name__ == '__main__':
-    get_data()
+    all_data = get_data()
+    save_data(all_data)
