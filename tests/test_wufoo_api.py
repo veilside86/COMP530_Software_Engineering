1af3d18ec629e5f4b865dc9ca5c844daf6687a65
Junseok-Hur <78327059+Junseok-Hur@users.noreply.github.com>
2023-02-25 22:54:46 -0500

    Create checkbox testing

--- tests/test_wufoo_api.py
+++ tests/test_wufoo_api.py
@@ -1,6 +1,9 @@
+import pytest
 import wufoo_api as wa
+from wufoo_api_gui import MainWindow
 import mysql.connector
 from typing import Tuple
+from PySide6.QtCore import Qt
 
 
 # assign test_open_db for setup test database on the server
@@ -42,7 +45,33 @@ def test_database_has_data():
     assert count > 0
 
 
-# class TestCheckBox:
-#     def test_checkbox_checked(self):
-#         wa.display_gui(data)
+def test_get_checkbox_data():
+    conn, cursor = wa.open_db()
+    cursor.execute('''SELECT Entry_Id, Prefix, First_Name, Last_Name, Title, Organization_Name,
+                Email, Organization_Website, Phone, Interested_Opt1, Interested_Opt2, Interested_Opt3,
+                Interested_Opt4, Interested_Opt5, Interested_Opt6, Interested_Opt7, Collabo_Time_Opt1,
+                Collabo_Time_Opt2, Collabo_Time_Opt3, Collabo_Time_Opt4, Collabo_Time_Opt5, Permission FROM wufoo''')
+    results = cursor.fetchone()
 
+    return results
+
+
+@pytest.fixture
+def test_app(qtbot):
+    results = test_get_checkbox_data()
+    test_gui = wa.display_gui(results)
+    qtbot.addWidget(test_gui)
+
+    return test_gui
+
+
+def test_checkbox_checked(qtbot):
+    results = test_get_checkbox_data()
+    window = MainWindow(results)
+    window.show()
+
+    spy = qtbot.wait_signal(window.data_window.checkbox.stateChanged)
+    qtbot.mouseClick(window.data_window.checkbox, Qt.LeftButton)
+    spy.wait()
+
+    assert window.data_window.checkbox.isChecked()
